15b3954c0c77fcf0107d589e07294f03c039f8dd
JuanXiRP <juanxiruiz@gmail.com>
2026-06-05 18:21:12 +0200

    Asegurado claves 4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,6 +33,8 @@ android {
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
+        manifestPlaceholders["googleApiKey"] = apiToken
     }
 
     buildTypes {
